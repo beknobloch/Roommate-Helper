b9fbf8d96afb09f672151765a26747583f49faf0
Benjamin Knobloch <bknobloc@stevens.edu>
2023-12-03 11:56:13 -0500

    Added Value class to more accurately store and display dollar amounts

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import user
 import ledger
 import item
+from value import Value
 
 # ----------------------------- EXAMPLE -----------------------------
 
@@ -17,14 +18,11 @@ def calculate_totals(ldger, usr):
         if usr != itm.user_who_paid:
             continue
         # calculate the length of users who use the item.
-        # if user uses the item subtract 1 from the length (this will help calculate what is owed)
-        if usr in itm.users_who_use:
-            length = len(itm.users_who_use) - 1
-        else:
-            length = len(itm.users_who_use)
+        # find the number of users the item
+        length = len(itm.users_who_use)
         # if the user paid for that item, iterate through the people who use that item
         for person in itm.users_who_use:
-            # if we're at the person who paid, continue
+            # if we're at the person who paid, continue, since they've already paid their share
             if person == usr:
                 continue
             if person.username in usr.totals:
@@ -36,23 +34,23 @@ def calculate_totals(ldger, usr):
 
 
 # make a Noah user with username and balance
-Noah = user.User("Noah76", 50)
+Noah = user.User("Noah76", Value(50))
 
 # make a Marcos user with username and balance
-Marcos = user.User("Marcos50", 20)
+Marcos = user.User("Marcos50", Value(20))
 
 # make a Ben user with username and balance
-Ben = user.User("Ben10", 100)
+Ben = user.User("Ben10", Value(100))
 
 # use the pay_user function to pay marcos $10. Will not allow me to pay marcos more than my balance
-Noah.pay_user(10, Marcos)
+Noah.pay_user(Value(10), Marcos)
 
 # create a milk item that has a price and a list of users that use that item
-milk = item.Item("Milk", 5.00, Noah)
+milk = item.Item("Milk", Value(5.00), Noah)
 milk.users_who_use = [Marcos, Ben, Noah]
 
 # create an apple item that has a price and a list of users that use that item
-apple = item.Item("Apple", 3.00, Marcos)
+apple = item.Item("Apple", Value(3.00), Marcos)
 apple.users_who_use = [Noah, Ben]
 
 # instantiate a ledger with item_list filled by items
--- user.py
+++ user.py
@@ -14,10 +14,10 @@ class User:
     # this method transfers the amount to the balance of user_being_paid
     def pay_user(self, amount, user_being_paid):
         if self.balance > amount:
-            print(f"Noah (Balance: ${self.balance}) is paying ${amount} to Marcos (Balance: ${user_being_paid.balance})")
+            print(f"{self} (Balance: {self.balance}) is paying {amount} to {user_being_paid} (Balance: {user_being_paid.balance})")
             self.balance -= amount
-            print(f"{self}'s new balance: ${self.balance}")
+            print(f"{self}'s new balance: {self.balance}")
             user_being_paid.balance += amount
-            print(f"{user_being_paid}'s new balance: ${user_being_paid.balance}")
+            print(f"{user_being_paid}'s new balance: {user_being_paid.balance}")
         else:
             print("You do not have enough balance to transfer that amount")
