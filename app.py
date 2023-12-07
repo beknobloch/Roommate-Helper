f02f34c8e1b912bcd859545f9b7196899290210e
Noah Spina <142749402+NoahSpina@users.noreply.github.com>
2023-12-06 21:18:52 -0500

    Added page navigation and user page

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import json
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from user import User
 from item import Item
 from ledger import Ledger
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
-class Database(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(200), nullable=False)
-    balance = db.Column(db.Float, nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User %r>' % self.id
@@ -45,6 +46,35 @@ def index():  # put application's code here
     return render_template('index.html')
 
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    user_to_delete = Users.query.get_or_404(id)
+
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting that user"
+
+
+@app.route('/user', methods=['POST', 'GET'])
+def add_user():
+    if request.method == 'POST':
+        username_content = request.form['content']
+        new_user = Users(content=username_content)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your user'
+    else:
+        users = Users.query.order_by(Users.date_created).all()
+        return render_template('user.html', users=users)
+
+
 @app.route('/pass_data', methods=['POST'])
 def pass_data():
     result = ""
@@ -64,14 +94,14 @@ def pass_dict():
     return json.dumps([[key, value] for key, value in sample_dict.items()])
 
 
-@app.route('/retrieveData', methods=['POST']) 
-def retrieveData(): 
+@app.route('/retrieveData', methods=['POST'])
+def retrieveData():
     data = request.get_json()
     response = data['data']
     # prints data received
     print(f"Received data: {response}")
     updateItemList(response)
-    
+
     # returns message (not needed)
     return jsonify({'message': 'Success'})
 
